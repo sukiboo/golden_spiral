d678c795f32306a8e84d41a2749a7f618c9f3e5b
Anton Dereventsov <dereventsov@gmail.com>
2019-11-07 14:14:05 -0500

    now saving variables

--- corput_sequence.py
+++ corput_sequence.py
@@ -5,13 +5,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.set_printoptions(precision=3, linewidth=115, suppress=True, formatter={'float':'{: 0.3f}'.format})
 import measure_efficiency as me
+import time
+start_time = time.time()
 
 
-num_pts = 1000
+num_pts = 1000000
 base = 2
 
 pts = np.array([])
 DD = np.array([])
+iter = 0
 for n in range(num_pts):
 	n_th_number, denom = 0., 1.
 	while n > 0:
@@ -20,6 +23,10 @@ for n in range(num_pts):
 		n_th_number += remainder / denom
 	pts = np.append(pts, n_th_number)
 	DD = np.append(DD, me.compute_discrepancy(pts))
+	iter += 1
+	print('iteration {:d}/{:d} completed, elapsed time: {:.2f}s'\
+		.format(iter, num_pts, time.time() - start_time))
+print('total elapsed time: {:.2f}s'.format(time.time() - start_time))
 
 
 plt.figure(figsize=(19,8))
@@ -29,6 +36,24 @@ x_pts = np.arange(num_pts) + 1
 ax.plot(x_pts, DD * x_pts, color='turquoise', label='nD*-discrepancy')
 Dy = np.log(x_pts) / (3*np.log(2)) + 1
 ax.plot(x_pts, Dy, color='crimson', label='theoretical estimate')
+
+ax.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+
+import pickle
+# #pickle.dump([num_pts, base, x_pts, DD, Dy], open('vars/1mil_vdc', 'wb'))
+# #num_pts, base, x_pts, DD, Dy = pickle.load(open('vars/1mil_vdc', 'rb'))
+
+
+plt.figure(figsize=(19,8))
+ax = plt.axes()
+
+Dy = np.log(x_pts) / (3*np.log(2)) + 1
+ax.plot(x_pts, DD * x_pts, color='turquoise', label='nD*-vdc')
+ax.plot(x_pts, DD2 * x_pts, color='dodgerblue', label='nD*-phi')
+ax.plot(x_pts, Dy, color='crimson', linewidth=2, label='theoretical estimate')
 
 ax.legend(loc='upper left')
 plt.tight_layout()
--- plot_Dg_n.py
+++ plot_Dg_n.py
@@ -5,15 +5,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.set_printoptions(precision=3, linewidth=115, suppress=True, formatter={'float':'{: 0.3f}'.format})
 import measure_efficiency as me
+import time
+start_time = time.time()
 
-gamma = (1 + 5**.5) / 2
-K_max = 1
 
-num_pts = 1000
-step_pts = 100
+phi = (1 + np.sqrt(5)) / 2
+gamma = phi
+a_max = 1
+
+num_pts = 1000000
+step_pts = 1
 
 x_pts = (1 + np.arange(num_pts)) * step_pts
-Dt = ((K_max + 1)**4 / 2 + K_max + 1) / x_pts
+K_max = 2*phi + .5 if gamma == phi else (a_max + 1)/2 * (a_max**2 + 2*a_max + 2)
+Dt = K_max / x_pts
 
 Dg = np.zeros(num_pts)
 Dm = np.zeros(num_pts)
@@ -26,6 +31,10 @@ for n in range(num_pts):
 	delta = pts[1:] - pts[:-1]
 	Dg[n] = np.max(delta)
 	Dm[n] = np.min(delta, initial=1, where=(delta > 1e-08))
+	print('iteration {:d}/{:d} completed, elapsed time: {:.2f}s'\
+		.format(n+1, num_pts, time.time() - start_time))
+print('total elapsed time: {:.2f}s'.format(time.time() - start_time))
+
 
 plt.figure(figsize=(19,8))
 ax = plt.axes()
@@ -51,11 +60,17 @@ plt.show()
 plt.figure(figsize=(19,8))
 ax = plt.axes()
 ax.plot(x_pts, DD * x_pts, color='turquoise', label='nD*-discrepancy')
-Dy = np.log(x_pts) / np.log(2) / 4 + 1
+Dy = np.log(x_pts) / (3*np.log(2)) + 1
 ax.plot(x_pts, Dy, color='crimson', label='theoretical estimate')
 ax.legend(loc='upper right')
 plt.tight_layout()
 plt.show()
+
+
+# save the variables
+import pickle
+pickle.dump([gamma, a_max, num_pts, step_pts, x_pts, K_max, Dt, Dg, Dm, DD, Dy], open('vars/1mil', 'wb'))
+#gamma, a_max, num_pts, step_pts, x_pts, K_max, Dt, Dg, Dm, DD, Dy = pickle.load(open('vars/1mil', 'rb'))
 
 
 '''
